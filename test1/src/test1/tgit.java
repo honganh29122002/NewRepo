57003d50a4ae939e936abc2d19ffb6236e3bb8af
lehonganh29122002 <lehonganh29122002@gmail.com>
2025-04-10 12:07:37 +0700

    Initial Commit.

--- test1/src/test1/tgit.java
+++ test1/src/test1/tgit.java
@@ -3,5 +3,11 @@ package test1;
 public class tgit {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		
 	}
 }
